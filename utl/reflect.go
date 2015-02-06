c2aa533275996fcf8665023724085cdaa1461910
ference <fym201@msn.com>
2015-02-06 15:47:37 +0800

    add map to struct support

--- utl/reflect.go
+++ utl/reflect.go
@@ -1,6 +1,7 @@
 package utl
 
 import (
+	"errors"
 	"reflect"
 	"strings"
 )
@@ -60,7 +61,7 @@ func StructTagMapByField(structPtr interface{}, tagType string, ignoreEmpty bool
 
 //将struct的指定字段转到map中
 //structPtr为struct的实例指针
-//mapObj为要存放数据的指针
+//mapObj为要存放数据的map
 //feilds为[]string类型代表要取的字段
 //feilds为map[string]string类型代表字段映射 {"structFeild":"mapKey"}, 如果mapKey为空则使用structFeild做为key
 func StructToMap(structPtr interface{}, mapObj map[string]interface{}, feilds interface{}) {
@@ -80,4 +81,42 @@ func StructToMap(structPtr interface{}, mapObj map[string]interface{}, feilds in
 			mapObj[v] = tp.FieldByName(v).Interface()
 		}
 	}
+}
+
+//将map根据指定的tag转为struct
+//structPtr为struct的实例指针
+//mapObj为要存放数据的map
+//tagType 为struct中要用的tag
+func MapToStruct(mapObj interface{}, structPtr interface{}, tagType string) error {
+	if reflect.TypeOf(mapObj).Kind() != reflect.Map {
+		return errors.New("mapObj is not a map")
+	}
+	m := mapObj.(map[string]interface{})
+	vl := reflect.ValueOf(structPtr).Elem()
+	tp := reflect.TypeOf(structPtr).Elem()
+
+	if vl.Kind() != reflect.Ptr || vl.IsNil() {
+		return errors.New("structPtr is not a struct instance pointer")
+	}
+
+	for i := 0; i < tp.NumField(); i++ {
+		f := tp.Field(i)
+		name := f.Name
+		tag := f.Tag.Get(tagType)
+
+		if tag == "-" {
+			continue
+		}
+		if tag == "" {
+			tag = strings.ToLower(name[0:1]) + name[1:]
+		}
+		vf := vl.Field(i)
+		if vf.CanSet() {
+			if v, ok := m[tag]; ok {
+				vf.Set(reflect.ValueOf(v))
+			}
+
+		}
+	}
+	return nil
 }
