5412b9c4946342283b9a6275fca75bccd96a7754
ference <ference@localhost.localdomain>
2015-03-11 15:51:24 +0800

    add utl/io.go

---
+++ utl/io.go
@@ -0,0 +1,28 @@
+package utl
+
+import (
+	"io"
+)
+
+func Pipe(in io.Reader, out io.Writer) error {
+	buf := make([]byte, 2048)
+	for {
+		n, err := in.Read(buf)
+
+		if n > 0 {
+			_, err = out.Write(buf)
+		}
+		if err != nil && err != io.EOF {
+			return err
+		}
+	}
+	return nil
+}
+
+func PipeAndClose(in io.ReadCloser, out io.WriteCloser) error {
+	defer func() {
+		in.Close()
+		out.Close()
+	}()
+	return Pipe(in, out)
+}
